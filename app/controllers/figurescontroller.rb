cdb96a70ca4a13ea65d0fb97244a7837f8aad9c3
Jacqueline Lee <anginblu@gmail.com>
2018-01-11 20:34:03 +0000

    Done.

--- app/controllers/figurescontroller.rb
+++ app/controllers/figurescontroller.rb
@@ -5,24 +5,42 @@ class FiguresController < ApplicationController
     erb :'/figures/index'
   end
 
-  get '/figures/:slug' do
-    @figure = Figure.find_by_slug(params[:slug])
-    erb :'/figures/show'
-  end
-
   get '/figures/new' do
     erb :'/figures/new'
   end
 
-  post '/figures' do
-    @figure = Figure.create(params[:figure][:name])
-    if params[:title][:name].empty?
-      @figure.figure_titles.create(params[:figure][:title_ids])
+  post '/figures/new' do
+
+    @figure = Figure.create(name: params["figure_name"])
+
+    if params["new_title"].empty?
+      params[:figure][:title_ids].each do |id|
+        @title = Title.find(id)
+        @figure.figure_titles.create(title: @title)
+      end
     else
-      @figure.figure_titles.create(params[:title][:name])
+      @title = Title.create(name: params["new_title"])
+      @figure.figure_titles.create(title: @title)
     end
     @figure.save
+
+    if params["new_landmark"].empty?
+      params[:figure][:landmark_ids].each do |id|
+        @landmark = Landmark.find(id)
+        @figure.landmarks << @landmark
+      end
+    else
+      @landmark = Landmark.create(name: params["new_landmark"])
+      @figure.landmarks << @landmark
+    end
+    @figure.save
+
+    redirect to "/figures/#{@figure.slug}"
   end
 
+  get '/figures/:slug' do
+    @figure = Figure.find_by_slug(params[:slug])
+    erb :'/figures/show'
+  end
 
 end
